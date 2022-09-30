2787723b7d5eea4e822c57271d9efd3fc062bff4
Hathaway-Shan <hathaway.shan@gmail.com>
2022-09-29 17:04:32 -0700

    todos are being added to supabase data is the correct shape

--- src/components/Todos/Todos.js
+++ src/components/Todos/Todos.js
@@ -1,15 +1,35 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { UserContext } from '../../context/UserContext';
+//todos imports
+import { useTodos } from '../../hooks/useTodos';
+import { createListItem } from '../../services/todos';
 
 export default function Todos() {
+  const [todo, setTodo] = useState('');
+  const { todos, setTodos } = useTodos();
+
   const { user } = useContext(UserContext);
 
   if (!user) {
     return <Redirect to="/auth/sign-in" />;
   }
 
-  return <div>We are now in todos</div>;
-}
+  const handleNewItem = async () => {
+    try {
+      const newTodo = await createListItem(todo);
+      setTodos((prev) => [...prev, newTodo]);
+      setTodo('');
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(e.message);
+    }
+  };
 
-UserContext;
+  return (
+    <div>
+      <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} />
+      <button onClick={handleNewItem}>Add Todo</button>
+    </div>
+  );
+}
--- src/services/todos.js
+++ src/services/todos.js
@@ -1,6 +1,15 @@
 import { checkError, client } from './client';
 
 export async function getListItems() {
-  const response = await client.from().select();
+  const response = await client.from('todos').select('*');
+  return response.data;
+}
+
+export async function createListItem(description) {
+  const response = await client
+    .from('todos')
+    .insert([{ description }])
+    .order('id', { ascending: true })
+    .single();
   return checkError(response);
 }
